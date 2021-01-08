e7c8cd39c3a53a513fd8ffb86c2d214b2cc6e311
Aseem Athale <athaleaseem@gmail.com>
2021-01-08 20:11:32 +0530

    Fixed color font typo.

--- drw.c
+++ drw.c
@@ -136,8 +136,6 @@ xfont_create(Drw *drw, const char *fontname, FcPattern *fontpattern)
 		die("no font specified.");
 	}
 
-	}
-
 	font = ecalloc(1, sizeof(Fnt));
 	font->xfont = xfont;
 	font->pattern = pattern;
