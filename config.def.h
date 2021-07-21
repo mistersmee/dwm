2676b8aa53724133bc6089695cec56bd5bc4442f
Aseem Athale <athaleaseem@gmail.com>
2021-07-21 17:20:17 +0530

    config.def.h: Use correct font
    Signed-off-by: Aseem Athale <athaleaseem@gmail.com>

--- config.def.h
+++ config.def.h
@@ -11,7 +11,6 @@ static const unsigned int gappiv    = 10;       /* vert inner gap between window
 static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
 static const unsigned int gappov    = 10;       /* vert outer gap between windows and screen edge */
 static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
-static char *fonts[]          = { "Fira Code:size=12", "JoyPixels:pixelsize=10:antialias=true:autohint=true"  };
 static const unsigned int baralpha = 0xd0;
 static const unsigned int borderalpha = OPAQUE;
 static const unsigned int alphas[][3]      = {
@@ -23,7 +22,8 @@ static unsigned int borderpx  = 1;        /* border pixel of windows */
 static unsigned int snap      = 32;       /* snap pixel */
 static int showbar            = 1;        /* 0 means no bar */
 static int topbar             = 1;        /* 0 means bottom bar */
-static char font[]            = "monospace:size=10";
+static char font[]            = "Fira Code:size=12";
+static char *fonts[]          = { "Fira Code:size=12", "JoyPixels:pixelsize=10:antialias=true:autohint=true"  };
 static char dmenufont[]       = "monospace:size=10";
 static char normbgcolor[]           = "#222222";
 static char normbordercolor[]       = "#444444";
