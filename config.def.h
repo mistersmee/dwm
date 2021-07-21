8e71675cde9a5eb60ba00f3a9b36cd29ffc874bb
Aseem Athale <athaleaseem@gmail.com>
2021-07-21 17:03:44 +0530

    config.def.h: Remove hardcoded dmenu options
    Signed-off-by: Aseem Athale <athaleaseem@gmail.com>

--- config.def.h
+++ config.def.h
@@ -78,7 +78,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbordercolor, "-sf", selfgcolor, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char scratchpadname[] = "scratchpad";
 static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, NULL };
