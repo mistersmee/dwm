c966bca2e4a4328ecae6c92cb3b7525e1eb5abfa
Aseem Athale <athaleaseem@gmail.com>
2021-10-04 16:40:29 +0530

    config.h: Switch to kitty as default terminal
    I use kitty as my main terminal now, replacing st. It does everything
    st did, but slightly better. So, changed defaults to kitty.

--- config.def.h
+++ config.def.h
@@ -49,6 +49,7 @@ static const Rule rules[] = {
 	{ "Firefox", NULL,     NULL,           1 << 8,    0,          0,          -1,        -1 },
 	{ "firefox", NULL,     NULL,           1 << 8,    0,          0,          -1,        -1 },
 	{ "St",      NULL,     NULL,           0,         0,          1,           0,        -1 },
+	{ "kitty",   NULL,     NULL,           0,         0,          1,           0,        -1 },
 	{ NULL,      NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
 };
 
@@ -79,9 +80,9 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { "kitty", NULL };
 static const char scratchpadname[] = "scratchpad";
-static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, NULL };
+static const char *scratchpadcmd[] = { "kitty", "-T", scratchpadname, NULL };
 /*
  * Xresources preferences to load at startup
  */
