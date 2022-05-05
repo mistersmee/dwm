e418bcf47456aa07f36029a282892cea49a29cf6
Aseem Athale <athaleaseem@gmail.com>
2022-05-05 21:02:57 +0530

    scratchpads: Move keybinding to mod+minus
    The single scratchpad patch had such a keybind, and I got used to that,
    so switching one of the scratchpads to that keybind.

--- config.def.h
+++ config.def.h
@@ -162,7 +162,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
-	{ MODKEY,                       XK_y,      togglescratch,  {.ui = 0 } },
+	{ MODKEY,                       XK_minus,  togglescratch,  {.ui = 0 } },
 	{ MODKEY,                       XK_u,      togglescratch,  {.ui = 1 } },
 	{ MODKEY,                       XK_x,      togglescratch,  {.ui = 2 } },
 	TAGKEYS(                        XK_1,                      0)
